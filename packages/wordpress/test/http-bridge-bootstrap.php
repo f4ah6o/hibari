8f35d478c7aed061da8aa8a35b44f0833a38400d
f12o <14793610+f4ah6o@users.noreply.github.com>
2026-08-22 06:41:12 +0900

    test: bootstrap WordPress HTTP bridge

---
+++ packages/wordpress/test/http-bridge-bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once HIBARI_WORDPRESS_ROOT . '/src/WordPressSqlTranslator.php';
+require_once HIBARI_WORDPRESS_ROOT . '/src/HttpBridge.php';
+
+$GLOBALS['hibari_wordpress_bridge_factory'] = function () {
+    if (!defined('HIBARI_RUNTIME_HTTP_URL')) {
+        throw new RuntimeException('HIBARI_RUNTIME_HTTP_URL is required for the HTTP bridge proof.');
+    }
+    return new \Hibari\WordPress\HttpBridge(HIBARI_RUNTIME_HTTP_URL);
+};
