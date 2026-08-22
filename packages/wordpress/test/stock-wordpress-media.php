153288f3aa10f2c77d77e7f30f3295d9538e3a12
f12o <14793610+f4ah6o@users.noreply.github.com>
2026-08-23 08:53:04 +0900

    test: load WordPress revision helpers for media proof

--- packages/wordpress/test/stock-wordpress-media.php
+++ packages/wordpress/test/stock-wordpress-media.php
@@ -72,6 +72,7 @@ $core_files = array(
     'wp-includes/kses.php',
     'wp-includes/cron.php',
     'wp-includes/post.php',
+    'wp-includes/revision.php',
     'wp-includes/class-wp-post-type.php',
     'wp-includes/class-wp-post.php',
     'wp-includes/nav-menu.php',
